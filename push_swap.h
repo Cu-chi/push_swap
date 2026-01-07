b2a965afa96d62e382b9dc3b19c8560b405b170b
Cu-chi <110144918+Cu-chi@users.noreply.github.com>
2026-01-07 14:51:08 +0100

    dev(main): args management start

---
+++ push_swap.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: equentin <equentin@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/07 14:07:13 by equentin          #+#    #+#             */
+/*   Updated: 2026/01/07 14:39:18 by equentin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include "ft_printf.h"
+# include "libft/libft.h"
+
+typedef struct s_arguments
+{
+	char	simple;
+	char	medium;
+	char	complex;
+	char	adaptive;
+	char	bench;
+} t_arguments
+
+#endif
