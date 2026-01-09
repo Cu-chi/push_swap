e107669dd6f372058637e85cc139b9ca68d5ccdb
Cu-chi <110144918+Cu-chi@users.noreply.github.com>
2026-01-09 13:57:25 +0100

    dev(main): args parsing

---
+++ utils.c
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: equentin <equentin@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/09 10:11:13 by equentin          #+#    #+#             */
+/*   Updated: 2026/01/09 12:53:23 by equentin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	free_stack_list(char **stack_list)
+{
+	int	i;
+
+	i = 0;
+	while (stack_list[i] != NULL)
+		free(stack_list[i++]);
+	free(stack_list);
+}
+
+int	exit_safe(char **stack_list, t_stacks *stacks, int fail)
+{
+	if (stack_list)
+		free_stack_list(stack_list);
+	free_stacks(stacks);
+	ft_printf("Error\n");
+	exit(fail);
+	return (0);
+}
+
+int	ft_atoi_ovrflw(const char *nptr, char **stack_list, t_stacks *stacks)
+{
+	int				i;
+	unsigned int	n;
+	int				m;
+
+	i = 0;
+	n = 0;
+	m = 1;
+	while ((nptr[i] >= 9 && nptr[i] <= 13) || nptr[i] == 32)
+		i++;
+	if (nptr[i] == '+' || nptr[i] == '-')
+		if (nptr[i++] == '-')
+			m = -1;
+	if (nptr[i] < '0' || nptr[i] > '9')
+		return (exit_safe(stack_list, stacks, EXIT_FAILURE));
+	while (nptr[i] >= '0' && nptr[i] <= '9')
+	{
+		n = n * 10 + nptr[i++] - '0';
+		if ((m == 1 && n > 2147483647) || (m == -1 && n > 2147483648))
+			return (exit_safe(stack_list, stacks, EXIT_FAILURE));
+	}
+	return (n * m);
+}
